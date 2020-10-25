2b49f152b313ad63287d49e5f82aa1d60ce12d9e
Sam Holt <samgholt20@gmail.com>
2020-10-25 17:47:08 +0800

    initials based brownlow votes

---
+++ initials_brownlow_total.R
@@ -0,0 +1,61 @@
+library('fitzRoy')
+
+library(tidyverse)
+
+afttable <- fitzRoy::get_afltables_stats()
+
+afttable_df <- afttable %>% as_tibble()
+
+afttable_df_sub <- afttable_df %>% 
+  select(First.name,Surname,Brownlow.Votes) %>% 
+  filter(Brownlow.Votes>0) %>% 
+  mutate(First.init = toupper(str_sub(First.name, 1, 1)),
+         Second.init = toupper(str_sub(Surname, 1, 1)))
+
+initials_votes <- afttable_df_sub %>% 
+  group_by(First.init, Second.init) %>% 
+  summarise(Total.votes = sum(Brownlow.Votes, na.rm = TRUE)) %>% 
+  arrange(desc(Total.votes))
+
+bottom_initials <- initials_votes %>% 
+  arrange(Total.votes)
+
+top_names <- afttable_df_sub %>% 
+  filter(First.init == initials_votes$First.init[1] &
+           Second.init == initials_votes$Second.init[1]) %>% 
+  group_by(First.name, Surname) %>% 
+  summarise(Total.votes = sum(Brownlow.Votes, na.rm = T)) %>% 
+  arrange(desc(Total.votes))
+
+second_names <- afttable_df_sub %>% 
+  filter(First.init == initials_votes$First.init[2] &
+           Second.init == initials_votes$Second.init[2]) %>% 
+  group_by(First.name, Surname) %>% 
+  summarise(Total.votes = sum(Brownlow.Votes, na.rm = T)) %>% 
+  arrange(desc(Total.votes))
+
+third_names <- afttable_df_sub %>% 
+  filter(First.init == initials_votes$First.init[3] &
+           Second.init == initials_votes$Second.init[3]) %>% 
+  group_by(First.name, Surname) %>% 
+  summarise(Total.votes = sum(Brownlow.Votes, na.rm = T)) %>% 
+  arrange(desc(Total.votes))
+
+bottom_names <- afttable_df_sub %>% 
+  filter(First.init == bottom_initials$First.init[1] &
+           Second.init == bottom_initials$Second.init[1] | 
+           First.init == bottom_initials$First.init[2] &
+           Second.init == bottom_initials$Second.init[2] | 
+           First.init == bottom_initials$First.init[3] &
+           Second.init == bottom_initials$Second.init[3] | 
+           First.init == bottom_initials$First.init[4] &
+           Second.init == bottom_initials$Second.init[4] | 
+           First.init == bottom_initials$First.init[5] &
+           Second.init == bottom_initials$Second.init[5] | 
+           First.init == bottom_initials$First.init[6] &
+           Second.init == bottom_initials$Second.init[6] | 
+           First.init == bottom_initials$First.init[7] &
+           Second.init == bottom_initials$Second.init[7]) %>% 
+  group_by(First.name, Surname) %>% 
+  summarise(Total.votes = sum(Brownlow.Votes, na.rm = T)) %>% 
+  arrange(desc(Total.votes))
