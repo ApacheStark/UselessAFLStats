396c553925d1c7f2332f5d5ad0cde1b9619f5bb8
Sam Holt <samgholt20@gmail.com>
2020-10-26 19:23:30 +0800

    deletes

--- most_games_no_vote.R
+++ most_games_no_vote.R
@@ -30,9 +30,3 @@ no_achiever <- afttable_tbl %>%
   arrange(desc(n))
 
 no_achiever
-
-afttable_tbl %>% 
-  filter(Name == 'Kevin Bartlett') %>% 
-  select(Brownlow.Votes) %>% 
-  ggplot(., aes(x = Brownlow.Votes)) +
-    geom_density()
