0023db6cbd047da2602f31a09866807567b2fb4e
Sam Holt <samgholt20@gmail.com>
2020-10-29 19:39:10 +0800

    gaz visual and best timeline

--- gaz_highest_avg_disp_ground.R
+++ gaz_highest_avg_disp_ground.R
@@ -7,7 +7,7 @@ afttable <- fitzRoy::get_afltables_stats(start_date = '2001-01-01')
 
 gaz <- afttable %>% 
   filter(ID == 1105) %>% 
-  select(ID, Date, First.name, Surname, Kicks, Handballs, Venue) %>% 
+  select(ID, Date, First.name, Surname, Kicks, Handballs, Playing.for, Venue) %>% 
   mutate(Disposals = Kicks + Handballs) %>% 
   arrange((Date))
 
@@ -33,3 +33,23 @@ gaz100last %>%
   summarise(Games.Played = n(),
             Avg.Disposals = mean(Disposals))  %>% 
   arrange(desc(Avg.Disposals))
+
+
+gas125to250 <- gaz[125:250,]
+gas125to250 %>% 
+  group_by(Venue) %>% 
+  summarise(Games.Played = n(),
+            Avg.Disposals = mean(Disposals))  %>% 
+  arrange(desc(Avg.Disposals))
+
+
+ggplot(gaz %>% mutate(Game.No = 1:n()), 
+       aes(x = Game.No, y = Disposals, col = Playing.for)) +
+  geom_point() + 
+  geom_smooth() +
+  geom_line(alpha = 0.2) +
+  scale_color_manual(values = c('#112277', '#dd3311')) +
+  theme_bw() +
+  labs(
+    title = 'Gary Ablett Jnr Disposals per Game'
+  )
